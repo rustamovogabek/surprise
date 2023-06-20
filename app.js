0a9872fe9757f193927bec2bcbd953b9f935e025
rustamovogabek <127534467+rustamovogabek@users.noreply.github.com>
2023-06-20 12:26:34 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+let bohona = ["she's different", "she always understands me", "she has pretty eyes", "i learn tons of useful things from her", "she loves me","she is my ideal girl","we make future plans together", "she is my future 'W'"]
+function findExcuse () {
+    let r = Math.floor(Math.random()*bohona.length)
+    h3.innerText = bohona[r]
+    
+}
